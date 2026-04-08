23ca11573445753835611c58878b7093c38a2af5
Chovatiya Kaushal K. <77385928+Kaushal77777@users.noreply.github.com>
2026-04-08 17:59:16 +0200

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        //Bucket sort is the most optimum solution we can find
+        unordered_map<int,int> mp;
+        vector<int> res(k);
+        for(int i:nums){
+            mp[i]++;
+        }
+        for(int i=0;i<k;i++){
+            int maxfreq=0,maxkey=-1;
+            for(auto p: mp){
+                if(p.second>=maxfreq){
+                    maxfreq=p.second;
+                    maxkey=p.first;
+                }
+            }
+            res[i]=maxkey;
+            mp.erase(maxkey);
+        }
+        return res;
+    }
+};
